2f26f812bfbcaaa34674e2570a88fb8b527affb1
dongdonglin(林向东) <dongdonglin@tencent.com>
2018-08-10 15:53:00 +0800

    m

--- ci/init_mysql_data.sh
+++ ci/init_mysql_data.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 # Run tests
 
+DB_CONTEXT='mysql -h127.0.0.1 -uroot'
 echo $DIR, `pwd`
 
-echo "create database metis"
-echo "show databases"|mysql -h127.0.0.1 -uroot
+echo "create database metis"|${DB_CONTEXT}
+echo "show databases"|${DB_CONTEXT}
